8c4b722d9839a47a5e73e6a925100ea805a19ccb
Nouman Sakhawat <rnouman28@gmail.com>
2020-03-07 21:08:54 +0500

    .

---
+++ android/app/src/main/java/com/rn_date_to_timestamp/MainActivity.java
@@ -0,0 +1,15 @@
+package com.rn_date_to_timestamp;
+
+import com.facebook.react.ReactActivity;
+
+public class MainActivity extends ReactActivity {
+
+  /**
+   * Returns the name of the main component registered from JavaScript. This is used to schedule
+   * rendering of the component.
+   */
+  @Override
+  protected String getMainComponentName() {
+    return "RN_Date_to_Timestamp";
+  }
+}
